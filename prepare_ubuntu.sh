f5b387aecf7220a32a3a073f3f6c591b56c39efd
Vadim <imamatory@gmail.com>
2019-04-05 22:57:56 +0000

    Fix ubuntu initial script

--- prepare_ubuntu.sh
+++ prepare_ubuntu.sh
@@ -1,1 +1,14 @@
+sudo apt-get install -y libfontconfig sudo
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+sudo locale-gen en_US.UTF-8
+
+sudo bash -c "cat >> ~/.bashrc" << EOM
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=C
+export LANGUAGE=en_US:en 
+
+\$TERMINFO=~/.local/kitty.app/share/terminfo/x/xterm-kitty bash -i
+EOM
+
